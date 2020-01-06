630184826a750e519c72ed0f3fd83c6c2f4f218f
stas1 <stas1@DESKTOP-Q2ECTOR>
2020-01-05 18:44:54 -0600

    Completed CRUD tests

--- restAsssuredProject/src/test/java/restAPI/CRUD_Test.java
+++ restAsssuredProject/src/test/java/restAPI/CRUD_Test.java
@@ -9,9 +9,9 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class CRUD_Test {
-
-	@Test(priority = 1, groups = { "positive" })
-	public void postTest() {
+	
+	@Test(priority = 1, description = "PostMethodPositiveTest", groups = { "positive" })
+	public void createTest() {
 
 		RequestSpecification request = RestAssured.given();
 
@@ -28,10 +28,49 @@ public class CRUD_Test {
 
 		int code = response.getStatusCode();
 
-		Assert.assertEquals(code, 201);
+		Assert.assertEquals(code, 201);		
+		
+		System.out.println("Response time for Post is "+response.getTime());
+	}
+	
+	@Test(priority = 2, description = "GetMethodPositiveTest", groups = { "positive" })
+	public void readTest() {
+
+		RequestSpecification request = RestAssured.given();
+
+		Response response = request.get("http://localhost:3000/posts/3");
+
+		int code = response.getStatusCode();
+
+		Assert.assertEquals(code, 200);
+		
+		System.out.println("Response time for Get is "+response.getTime());		
+	}
+	
+	@Test(priority = 3, description = "PutMethodPositiveTest", groups = { "positive" })
+	public void updateTest() {
+
+		RequestSpecification request = RestAssured.given();
+
+		request.header("Content-Type", "application/json");
+
+		JSONObject json = new JSONObject();
+		json.put("id", "3");
+		json.put("title", "SeleniumTest");
+		json.put("author", "StanUpdated");
+
+		request.body(json.toJSONString());
+
+		Response response = request.put("http://localhost:3000/posts/3");
+
+		int code = response.getStatusCode();
+
+		Assert.assertEquals(code, 200);
+		
+		System.out.println("Response time for Put is "+response.getTime());
 	}
 
-	@Test(priority = 2, groups = { "positive" })
+	@Test(priority = 4, description = "DeleteMethodPositiveTest", groups = { "positive" })
 	public void deleteTest() {
 
 		RequestSpecification request = RestAssured.given();
@@ -41,6 +80,7 @@ public class CRUD_Test {
 		int code = response.getStatusCode();
 
 		Assert.assertEquals(code, 200);
+		
+		System.out.println("Response time for Delete is "+response.getTime());
 	}
-
 }
